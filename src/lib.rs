0efa565988688cf35eae0c289b38ae7e91f7f9f9
AthosOfAthos <athosofathos@gmail.com>
2023-03-18 13:09:33 -0500

    Added docs for Name methods

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #[cfg(feature = "serde")]
 mod serialize;
 
-/// A small fixed length string 
+/// A small fixed length string that's cheap to create and compare.
 #[repr(C, align(16))]
 #[derive(Clone, Copy)]
 pub struct Name {
@@ -15,6 +15,14 @@ const NULL: u8 = b'\0';
 impl Name {
     pub const LENGTH: usize = 16;
 
+    /// Creates a new Name from a string slice.
+    /// Name must be valid UTF8 and be between 1..16 bytes long.
+    /// # Example
+    /// ```
+    /// use str_id::Name;
+    /// const AIR_ID: Name = Name::new("air");
+    /// const STONE_ID: Name = Name::new("stone");
+    /// ```
     pub const fn new(name: &str) -> Self {
         assert!(name.len() > 0, "Failed to create Name: Cannot be empty");
         assert!(name.len() <= Self::LENGTH, "Failed to create Name: Length may not exceed 16");
@@ -30,6 +38,19 @@ impl Name {
         Name { array }
     }
 
+    /// Returns the length of internal string **in bytes**.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use str_id::Name;
+    ///
+    /// let fancy_foo = Name::new("ƒoo");
+    /// assert_eq!(4, fancy_foo.len());
+    ///
+    /// let bar = Name::new("bar");
+    /// assert_eq!(3, bar.len());
+    /// ```
     pub const fn len(&self) -> usize {
         let mut index = 0;
         while index <= Self::LENGTH {
@@ -41,6 +62,14 @@ impl Name {
         return Self::LENGTH;
     }
 
+    /// Returns a string slice representation of self.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let name = str_id::Name::new("foo");
+    /// assert_eq!("foo", name.as_str());
+    /// ```
     pub const fn as_str(&self) -> &str {
         // This assumes Name::new was fed a valid UTF8 str
         unsafe {
