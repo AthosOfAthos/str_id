66e6ba708b644987702271231921603b1f61665d
AthosOfAthos <athosofathos@gmail.com>
2023-05-17 18:24:58 -0500

    Added serde support for LongName

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,4 +1,4 @@
-use crate::Name;
+use crate::{Name, LongName};
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use serde::de::Visitor;
 
@@ -8,13 +8,13 @@ impl Serialize for Name {
     }
 }
 
-struct NameVisitor {}
+struct NameVisitor;
 
 impl<'de> Visitor<'de> for NameVisitor {
     type Value = Name;
 
     fn expecting(&self, formatter: &mut core::fmt::Formatter) -> core::fmt::Result {
-        write!(formatter, "A str between 1 and {} chars long with no null chars", Name::LENGTH)
+        write!(formatter, "A str between 1 and {} chars long with no NULL chars", Name::LENGTH)
     }
 
     fn visit_str<E: serde::de::Error>(self, name: &str) -> Result<Self::Value, E> {
@@ -29,5 +29,36 @@ impl<'de> Visitor<'de> for NameVisitor {
 impl<'de> Deserialize<'de> for Name {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
         deserializer.deserialize_str(NameVisitor {})
+    }
+}
+
+
+impl Serialize for LongName {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        serializer.serialize_str(self.as_str())
+    }
+}
+
+struct LongNameVisitor;
+
+impl<'de> Visitor<'de> for LongNameVisitor {
+    type Value = LongName;
+
+    fn expecting(&self, formatter: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(formatter, "A str between 1 and {} chars long with no NULL chars", LongName::LENGTH)
+    }
+
+    fn visit_str<E: serde::de::Error>(self, name: &str) -> Result<Self::Value, E> {
+        if name.is_empty() { return Err(E::custom("LongName cannot be empty")) }
+        if name.len() > LongName::LENGTH { return Err(E::custom("LongName too long")) }
+        if name.contains('\0') { return Err(E::custom("LongName may not contain null")) }
+        
+        Ok(LongName::new(name))
+    }
+}
+
+impl<'de> Deserialize<'de> for LongName {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        deserializer.deserialize_str(LongNameVisitor {})
     }
 }
